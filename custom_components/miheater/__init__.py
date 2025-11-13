3454ff90855dca8bac0d04462260bf9c947a2e9f
dkoto5 <155387801+dkoto5@users.noreply.github.com>
2025-11-13 22:30:15 +0100

    Update __init__.py
    corrected for newer HA

--- custom_components/miheater/__init__.py
+++ custom_components/miheater/__init__.py
@@ -1,25 +1,24 @@
-import asyncio
 import logging
+from homeassistant.const import Platform
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
 
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+PLATFORMS: list[Platform] = [Platform.CLIMATE]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Mi Heater from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "climate")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "climate")
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
-
+    unloaded = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unloaded:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unloaded
